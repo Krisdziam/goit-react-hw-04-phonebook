1f762239aa828161b519faeb7d790af001cfd0a1
Kristian <kris.dziamka@gmail.com>
2022-10-14 18:10:51 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,61 +1,62 @@
-import React from 'react';
+import {React, Component}from 'react';
 import styles from './Form.module.css';
+import { Component } from 'react';
 
 
 
-export default function Form({onSubmit}) {
+// export default function Form({onSubmit}) {
 
 
 
 
-  return (
-    <>
-      <h2 className={styles.subtitle}>Add new contact</h2>
-      <form
-        className={styles.form}
-        onSubmit={this.handleSubmitForm}
-      >
-        <div className={styles.formContainer}>
-          <div>
-            <label className={styles.label}>Name:</label>
-            <input
-              className={styles.input}
-              placeholder="Enter name"
-              onChange={this.handleInputChange}
-              type="text"
-              name="name"
-              value={this.state.name}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. 
-              For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </div>
-          <div>
-            <label className={styles.label}>
-              Number:
-            </label>
-            <input
-              className={styles.input}
-              placeholder="Enter number"
-              onChange={this.handleInputChange}
-              type="tel"
-              name="number"
-              value={this.state.number}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, 
-              parentheses and can start with +"
-              required
-            />
-          </div>
-        </div>
-        <button type="submit" className={styles.addBtn}>
-          Add <br /> Contact
-        </button>
-      </form>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2 className={styles.subtitle}>Add new contact</h2>
+//       <form
+//         className={styles.form}
+//         onSubmit={this.handleSubmitForm}
+//       >
+//         <div className={styles.formContainer}>
+//           <div>
+//             <label className={styles.label}>Name:</label>
+//             <input
+//               className={styles.input}
+//               placeholder="Enter name"
+//               onChange={this.handleInputChange}
+//               type="text"
+//               name="name"
+//               value={this.state.name}
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               title="Name may contain only letters, apostrophe, dash and spaces. 
+//               For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//               required
+//             />
+//           </div>
+//           <div>
+//             <label className={styles.label}>
+//               Number:
+//             </label>
+//             <input
+//               className={styles.input}
+//               placeholder="Enter number"
+//               onChange={this.handleInputChange}
+//               type="tel"
+//               name="number"
+//               value={this.state.number}
+//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//               title="Phone number must be digits and can contain spaces, dashes, 
+//               parentheses and can start with +"
+//               required
+//             />
+//           </div>
+//         </div>
+//         <button type="submit" className={styles.addBtn}>
+//           Add <br /> Contact
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
 
 
 
@@ -74,7 +75,7 @@ export default class Form extends Component {
 
   handleSubmitForm = e => {
     e.preventDefault();
-    onSubmit(this.state);
+    this.props.onSubmit(this.state);
     this.reset();
   };
 
